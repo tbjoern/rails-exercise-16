11477cd52e02448a9eb1e80b0bf0585d47eecd4d
Soeren Tietboehl <soeren.tietboehl@student.hpi.de>
2016-10-25 18:16:20 +0200

    feat(author#index): Change new author to add author
    closes #6

--- spec/views/authors/index.html.erb_spec.rb
+++ spec/views/authors/index.html.erb_spec.rb
@@ -21,4 +21,10 @@ RSpec.describe "authors/index", type: :view do
     assert_select "tr>td", :text => "First Name Last Name".to_s, :count => 2
     assert_select "tr>td", :text => "Homepage".to_s, :count => 2
   end
+
+  it "has a link to add author" do
+    render
+
+    assert_select '<a href=?', text: 'Add author'
+  end
 end
