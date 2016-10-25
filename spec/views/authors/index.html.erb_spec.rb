23b652ca047c6892bbb2d7b1329eea42dd559e94
Soeren Tietboehl <soeren.tietboehl@student.hpi.de>
2016-10-25 18:02:31 +0200

    feat(author#index) Add homepage to listing
    closes #4 closes #5

--- spec/views/authors/index.html.erb_spec.rb
+++ spec/views/authors/index.html.erb_spec.rb
@@ -19,6 +19,6 @@ RSpec.describe "authors/index", type: :view do
   it "renders a list of authors" do
     render
     assert_select "tr>td", :text => "First Name Last Name".to_s, :count => 2
-    #assert_select "tr>td", :text => "Homepage".to_s, :count => 2
+    assert_select "tr>td", :text => "Homepage".to_s, :count => 2
   end
 end
